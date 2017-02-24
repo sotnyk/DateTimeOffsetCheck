6c40762b95499f380803290809c50ebdf51e4a3f
ssotnyk <sergei.sotnik@gmail.com>
2017-02-24 17:07:05 +0200

    with DB

--- DateTimeOffsetCheck/DateTimeOffsetCheck/Program.cs
+++ DateTimeOffsetCheck/DateTimeOffsetCheck/Program.cs
@@ -9,16 +9,78 @@ namespace DateTimeOffsetCheck
 {
     class Program
     {
+        static Database _db;
+
         static void Main(string[] args)
         {            
-            CheckDTOBehaviour();
+            //CheckDTOBehaviour();
             CheckDtoInDB();
             Console.ReadLine();
         }
 
         private static void CheckDtoInDB()
         {
-            var db = new Database();
+            _db = new Database();
+            CreateTable();
+            PutValues();
+            WriteValues();
+        }
+
+        private static void WriteValues()
+        {
+            Console.WriteLine();
+            Console.WriteLine("As datetime model");
+            foreach (var item in _db.Fetch<DateModel>(@"SELECT * FROM ""Dates"""))
+            {
+                Console.WriteLine($"Timestamp:{item.Timestamp}, TimestampZ:{item.TimestampZ}, {item.Description}");
+                Console.WriteLine($"Kind:{item.Timestamp.Kind}, Kind:{item.TimestampZ.Kind}");
+            }
+        }
+
+        private static void PutValues()
+        {
+            DateTime now = DateTime.UtcNow;
+            var model = new DateModel()
+            {
+                Timestamp = now,
+                TimestampZ = now,
+                Description = "DateTime",
+            };
+            _db.Insert("Dates", model);
+
+            DateTimeOffset nowZ = new DateTimeOffset(2017, 2, 24, 12, 12, 12, TimeSpan.FromHours(2));
+            var modelZ = new DateModelZ()
+            {
+                Timestamp = nowZ,
+                TimestampZ = nowZ,
+                Description = "DateTimeOffset",
+            };
+            _db.Insert("Dates", modelZ);
+
+            DateTimeOffset nowZoff = new DateTimeOffset(2017, 2, 24, 12, 12, 12, TimeSpan.FromHours(5));
+            var modelZoff = new DateModelZ()
+            {
+                Timestamp = nowZoff,
+                TimestampZ = nowZoff,
+                Description = "DateTimeOffset",
+            };
+
+            _db.Insert("Dates", modelZoff);
+        }
+
+        private static void CreateTable()
+        {
+            _db.Execute(@"
+DROP TABLE IF EXISTS ""Dates"";
+
+CREATE TABLE ""Dates"" (
+    ""Id"" UUID NOT NULL PRIMARY KEY,
+    ""Timestamp"" TIMESTAMP NOT NULL,
+    ""TimestampZ"" TIMESTAMP WITH TIME ZONE NOT NULL,
+    ""Description"" TEXT
+);
+            ");
+            Console.WriteLine("Table Dates has created.");
         }
 
         private static void CheckDTOBehaviour()
@@ -45,4 +107,21 @@ namespace DateTimeOffsetCheck
             Console.WriteLine($"now.UtcDateTime==utcnow.UtcDateTime: {now.UtcDateTime == utcnow.UtcDateTime}");
         }
     }
+
+    public class DateModelZ
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTimeOffset Timestamp { get; set; }
+        public DateTimeOffset TimestampZ { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class DateModel
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime Timestamp { get; set; }
+        public DateTime TimestampZ { get; set; }
+        public string Description { get; set; }
+    }
+
 }
